66c0f757a884d5bf4a97bf1d3de297fac2e3adb9
Zeyd Alabsi <gotozeyd336@gmail.com>
2019-05-20 22:33:54 +0000

    third edit

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
+// *************************** YOUR CODE BELOW *******************************
+//******************TEST EARLY AND OFTEN USING console.log() ******************
+//****************** SERIOUSLY TEST USING console.log()!!! ******************
+/* global $ */
 
 
-
-
-
-
-
-
-$("select.options").change(function(){
-    var change = $(this).children("option:selected").val();
-     $("#main").css("background-color","black");
+$("#search-button").click(function(){
+    console.log('clicked')
+    
+    var searchTerm = $("#search-term").val();
+    
+    var api_url = "https://api.soundcloud.com/tracks?q="+searchTerm+"&client_id=5aa8e389ba4e24b6106af5159ab3e344"
+    
+   $.ajax({
+        url: api_url,
+        method: "GET",
+        success: function(response) {
+            console.log(response);
+            
+            $(".container--results").html('');
+            
+            var title = response[0].title
+            
+            $(".container--results").html(title);
+            
+        }
+    }); 
+  
 });
+
+
+
+
+
+
+
+//$("select.options").change(function(){
+//    var change = $(this).children("option:selected").val();
+//     $("#main").css("background-color","black");
+//});
